0f79b5a0c9be803750516d7b4118803480aabafb
Yelena Chamanova <ychamanova@gmail.com>
2019-10-28 17:52:51 -0700

    Implement detectNetwork function to tell one card network from another

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -11,8 +11,18 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
+  if (
+    cardNumber.length === 14 &&
+    (cardNumber.startsWith("38") || cardNumber.startsWith("39"))
+  ) {
+    console.log("Diner's Club");
+  } else if (
+    cardNumber.length === 15 &&
+    (cardNumber.startsWith("34") || cardNumber.startsWith("37"))
+  ) {
+    console.log("American Express");
+  } else {
+    console.log("Please provide correct credit card number");
+  }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
-
-
